86fc426a1bad0748b0ac372f306020bfa3cc649a
sherotree <wytreeshero@163.com>
2019-12-06 16:41:02 +0800

    so difficult

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,37 +3,41 @@ import './Calculator.css'
 
 export function Calculator() {
   const element = [
-    { AC:function handleReset(){}},
-    {'%': function percent(number) {
-      return number/100
-    }},
-{ "÷":'/' },
-{ 7:7 } , 
-{8:8},
-  {9:9},
-  {'×':'*'},
-    { 4:4},
-  {5:5},
-  {6:6},
-  {'-':'-'}
-  {1:1},
-  {2:2},
-  {3:3},
-  {'+':'+'},
+    { AC: function handleReset() {} },
+    {
+      '%': function percent(number) {
+        return number / 100
+      },
+    },
+    { '÷': '/' },
+    { 7: 7 },
+    { 8: 8 },
+    { 9: 9 },
+    { '×': '*' },
+    { 4: 4 },
+    { 5: 5 },
+    { 6: 6 },
+    { '-': '-' },
+    { 1: 1 },
+    { 2: 2 },
+    { 3: 3 },
+    { '+': '+' },
     { 0: 0 },
     { '.': '.' },
-    {'=':'='}]
-  const [show, setShow] = useState([])
+    { '=': '=' },
+  ]
+  const [show, setShow] = useState(null)
 
   function handleReset() {
     setShow([])
   }
 
-
   return (
     <div>
-      <p>{show.toString}</p>
-      
+      <p>{show}</p>
+      {element.map((item, index) => {
+        return <div key={index}>{item}</div>
+      })}
     </div>
   )
 }
